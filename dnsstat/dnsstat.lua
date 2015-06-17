c4cf2f339d18fc43825f05e667e3f9b6783699f4
Alexander W. Janssen <alexander.janssen@gmail.com>
2015-06-17 16:21:35 +0200

    kinda works, but datatype and comparison problems.

--- dnsstat/dnsstat.lua
+++ dnsstat/dnsstat.lua
@@ -1,18 +1,28 @@
 local request_hash = {}
 do
       dumpers = {}
+      local dns_resp_f = Field.new("dns.flags.response")
+      local time_epo_f = Field.new("frame.time_epoch")
+      local dns_id_f   = Field.new("dns.id")
       local function init_listener()
-            local tap = Listener.new("ip", "dns")
+            local tap = Listener.new("ip", "udp")
             function tap.packet(pinfo,tvb,ip)
-                  if (dns.flags.response == 0) then
-                        request_hash[dns.id] = frame.time_epoch
+                  local dns_flags_response = tostring(dns_resp_f())
+                  local frame_time_epoch = tostring(time_epo_f())
+                  local dns_id = tostring(dns_id_f())
+
+                  -- not sure why this equality check fails.
+                  if (dns_flags_response == 1) then
+                        request_hash[dns_id] = frame_time_epoch
                   end
-                  if (dns.flags.response == 1) and not (request_hash[dns.id] == nil) then
-                        local delta = frame.time_epoch - request_hash[dns.id]
+
+                  -- not sure why this equality check fails.
+                  if ((dns_flags_response == 0) and not (request_hash[dns_id] == nil)) then
+                        local delta = frame_time_epoch - request_hash[dns_id]
                         print(delta)
-                  else
-                        print(string.format("haven't seen dns.id %s\n", dns.id))
                   end
+
+                  print(string.format("debug: epoch: %s id: %s resp: %s", frame_time_epoch, dns_id, dns_flags_response))
             end
             function tap.reset()
                    for item,dumper in pairs(dumpers) do
